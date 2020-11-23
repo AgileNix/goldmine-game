121efad6eef2f77acabe1aeb2821f84dc3716194
AgileNix <vasyl.lagutin@agilenix.com>
2020-11-23 20:28:08 +0200

    added experimental flags

--- solution.js
+++ solution.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node --harmony-top-level-await --experimental-json-modules --no-warnings
+
 import { startMiningGame } from './gameEngine.js';
 import { term, config } from './constants.js';
 
